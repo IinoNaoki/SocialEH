e3b0c1574c947f04ad42283ed8ef7136ab5f133b
iinonaoki <iinonaoki@outlook.com>
2015-03-10 17:16:05 +0800

    update

--- Hello.py
+++ Hello.py
@@ -4,7 +4,6 @@ Created on 2 Mar, 2015
 @author: yzhang28
 '''
 
-
 '''
 S = (CS, Q_H, Q_L, MS)
 CS: 10 + 1 + 10 contact states
@@ -16,40 +15,71 @@ MS: 10
 
 import mdptoolbox
 import numpy as np
+from scipy.sparse import csr_matrix as sparse
 
 CON_CSize = 10 + 1 + 10
-
-CON_ESize = 10
-
-CON_QSize_H = 8
-CON_injec_high = 0.5
-CON_QSize_L = 8
-CON_injec_low = 0.5
-
 CON_MSize = 10
+CON_ESize = 10
+CON_QSize_H = 8
+CON_QSize_L = 8
+
+CON_CSize = 5 + 1 + 5
+CON_MSize = 5
+CON_ESize = 5
+CON_QSize_H = 2
+CON_QSize_L = 8
+
+CON_DIM = CON_CSize * CON_MSize * CON_ESize * CON_QSize_H * CON_QSize_L
+
+CON_injec_low = 0.8
+CON_injec_high = 0.1
 
 # define actions
-A = np.array([0,1,2])
 A_IDLE = 0 # 0: idle
 A_GETE = 1 # 1: get energy
 A_QH = 2 # 2: send Q_H
 A_QL = 3 # 3: send Q_L
+SET_A = [A_IDLE, A_GETE, A_QH, A_QL]
 #
 
-# matrix for contacts
-C_mat = np.zeros((CON_CSize,CON_CSize))
-C_mat[:] = 1.0/CON_CSize
+def Get_C_mat(act):
+    # matrix for contacts
+    C_mat = np.zeros((CON_CSize,CON_CSize))
+    C_mat[:] = 1.0/CON_CSize
+    return C_mat
 #
 
+def Get_M_mat(act):
+    # matrix for messenger
+    M_mat = np.zeros((CON_MSize,CON_MSize))
+    M_mat[:] = 1.0/CON_MSize
+    return M_mat
 
 #
-# def E_plus_mat(elen):
-# 
-# def E_minus
+def E_plus_mat(elen, chg=1):
+    _e_plus = np.array([[0.0 for _ in range(elen)] for _ in range(elen)])
+    for i in range(elen-1):
+        _e_plus[i][i+1] = 1.0
+    _e_plus[elen-1][elen-1] = 1.0
+    return _e_plus
 
-# def get_E_mat(act):
-#     if act == A_GETE:
-        
+def E_zero_mat(elen):
+    return np.identity(elen, float)
+
+def E_minus_mat(elen,dischg=1):
+    _e_minus = np.array([[0.0 for _ in range(elen)] for _ in range(elen)])
+    for j in range(1,elen):
+        _e_minus[j][j-1] = 1.0
+    _e_minus[0][0] = 1.0
+    return _e_minus
+
+def Get_E_mat(act):
+    if act == A_IDLE:
+        return E_zero_mat(CON_ESize)
+    elif act == A_GETE:
+        return E_plus_mat(CON_ESize)
+    elif act == A_QH or act == A_QL:
+        return E_minus_mat(CON_ESize)
 
 
 # matrix for Q high and Q low
@@ -71,7 +101,7 @@ def Q_minus_mat(qlen):
     _q_minus[0][0] = 1.0    
     return _q_minus
 
-def get_Q_mat(HorL, act):
+def Get_Q_mat(HorL, act):
     Q_H_mat_plus = Q_plus_mat(CON_QSize_H,CON_injec_high)
     Q_H_mat_zero = Q_zero_mat(CON_QSize_H)
     Q_H_mat_minus = Q_minus_mat(CON_QSize_H)
@@ -94,32 +124,55 @@ def get_Q_mat(HorL, act):
         exit('Error in get_Q_mat()') 
 
 
-# matrix for messenger
-M_mat = np.zeros((CON_MSize,CON_MSize))
-M_mat[:] = 1.0/CON_MSize
+
+def Get_Overall_mat(act):
+    _cmat = Get_C_mat(act)
+    _mmat = Get_M_mat(act)
+    _emat = Get_E_mat(act)
+    _qhmat = Get_Q_mat('H', act)
+    _qlmat = Get_Q_mat('L', act)
+    overkron = np.kron(np.kron(np.kron(_cmat, _mmat), _emat), np.kron(_qhmat,_qlmat))
+#     print overkron.shape
+    return overkron
 
 
-P = [ np.kron(Q_H_AisH,Q_H_AisL), np.kron(Q_L_AisH,Q_L_AisL) ]
+def Reward(_c,_m,_e,_qh,_ql, action):
+    if action == A_IDLE:
+        return 0.0
+    elif action == A_GETE and _c in [0,1,2,3,4]:
+        return -0.5
+    elif action == A_QH and _qh>0 and _c in [6,7,8,9,10]:
+        return 2.0
+    elif action == A_QL and _ql>0 and _c in [6,7,8,9,10]:
+        return 0.7
+    else:
+        return 0.0
 
-R_AH = np.array([0 for _ in range(CON_Q_high*CON_Q_low)])
-R_AL = np.array([0 for _ in range(CON_Q_high*CON_Q_low)])
-for qh in range(CON_Q_high):
-    for ql in range(CON_Q_low):
-        ind = int(qh*(CON_Q_low) + ql)
-        R_AH[ind] =  4.0 - 0.1*qh - 0.05*ql
-        R_AL[ind] =  2.0 - 0.1*qh - 0.05*ql
-        if qh==0:
-            R_AH[ind] = 0.0
-        if ql==0:
-            R_AL[ind] = 0.0
-R_AH = R_AH.reshape(CON_Q_high*CON_Q_low,1)    
-R_AL = R_AL.reshape(CON_Q_high*CON_Q_low,1)
-R = np.concatenate((R_AH, R_AL),axis=1)
+R = np.zeros((CON_DIM,len(SET_A)))
+cnt = 0
+for ic in range(CON_CSize):
+    for im in range(CON_MSize):
+        for ie in range(CON_ESize):
+            for iqh in range(CON_QSize_H):
+                for iql in range(CON_QSize_L):
+                    for action in SET_A:
+                        R[cnt][action] = Reward(ic,im,ie,iqh,iql, action)
+                    cnt = cnt + 1
+                    
+# R = np.random.randint(0,5,(CON_DIM,len(SET_A)))
+print R
+print R.shape
+
+P = np.array([ Get_Overall_mat(A_IDLE), Get_Overall_mat(A_GETE), Get_Overall_mat(A_QH), Get_Overall_mat(A_QL) ])
+# 
+# print P.shape
+# print P
+# print R.shape
 # print R
-
 
 vi = mdptoolbox.mdp.ValueIteration(P, R, 0.99)
 vi.run()
-print np.asarray(vi.policy).reshape(CON_Q_high,CON_Q_low)
-# print vi.iter
-# print np.asarray(vi.V).reshape(CON_Q_high,CON_Q_low)
+print "Done"
+# print vi.policy
+print vi.iter
+# print vi.V
