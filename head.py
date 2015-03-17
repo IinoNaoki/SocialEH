3855d8842af23e2407095e1fa364ef0de7f43694
iinonaoki <iinonaoki@outlook.com>
2015-03-17 09:08:29 +0800

    update

--- head.py
+++ head.py
@@ -14,12 +14,12 @@ SET_CHARGER = [1,2,3,4,5]
 SET_MESSENGER = [6,7,8,9,10]
 
 CON_CSize = len(SET_NOTHING) + len(SET_CHARGER) + len(SET_MESSENGER)
-CON_MSize = 5
+# CON_MSize = 5
 CON_ESize = 5
 CON_QSize_H = 2
 CON_QSize_L = 8
 
-CON_DIM = CON_CSize * CON_MSize * CON_ESize * CON_QSize_H * CON_QSize_L
+CON_DIM = CON_CSize * CON_ESize * CON_QSize_H * CON_QSize_L
 
 CON_injec_low = 0.8
 CON_injec_high = 0.1
@@ -37,61 +37,55 @@ def Get_C_mat(act):
     C_mat = np.zeros((CON_CSize,CON_CSize))
     C_mat[:] = 1.0/CON_CSize
     return C_mat
-#
 
-def Get_M_mat(act):
-    # matrix for messenger
-    M_mat = np.zeros((CON_MSize,CON_MSize))
-    M_mat[:] = 1.0/CON_MSize
-    return M_mat
-
-#
-def E_plus_mat(elen, chg=1):
-    _e_plus = np.array([[0.0 for _ in range(elen)] for _ in range(elen)])
-    for i in range(elen-1):
-        _e_plus[i][i+1] = 1.0
-    _e_plus[elen-1][elen-1] = 1.0
-    return _e_plus
-
-def E_zero_mat(elen):
-    return np.identity(elen, float)
-
-def E_minus_mat(elen,dischg=1):
-    _e_minus = np.array([[0.0 for _ in range(elen)] for _ in range(elen)])
-    for j in range(1,elen):
-        _e_minus[j][j-1] = 1.0
-    _e_minus[0][0] = 1.0
-    return _e_minus
 
 def Get_E_mat(act):
-    if act == A_IDLE:
-        return E_zero_mat(CON_ESize)
-    elif act == A_GETE:
-        return E_plus_mat(CON_ESize)
-    elif act == A_QH or act == A_QL:
-        return E_minus_mat(CON_ESize)
-
-
-# matrix for Q high and Q low
-def Q_plus_mat(qlen, inj):
-    _q_plus = np.array([[0.0 for _ in range(qlen)] for _ in range(qlen)])
-    for i in range(qlen-1):
-        _q_plus[i][i] = 1.0 - inj
-        _q_plus[i][i+1] = inj
-    _q_plus[qlen-1][qlen-1] = 1.0
-    return _q_plus
-
-def Q_zero_mat(qlen):
-    return np.identity(qlen, float)
+    #
+    def E_plus_mat(chg=1):
+        _e_plus = np.zeros((CON_ESize,CON_ESize))
+        for i in range(CON_ESize-1):
+            _e_plus[i][i+1] = 1.0
+        _e_plus[CON_ESize-1][CON_ESize-1] = 1.0
+        return _e_plus
     
-def Q_minus_mat(qlen):    
-    _q_minus = np.array([[0.0 for _ in range(qlen)] for _ in range(qlen)])
-    for j in range(1,qlen):
-        _q_minus[j][j-1] = 1.0
-    _q_minus[0][0] = 1.0    
-    return _q_minus
+    def E_zero_mat():
+        return np.identity(CON_ESize, float)
+    
+    def E_minus_mat(dischg=1):
+        _e_minus = np.zeros((CON_ESize,CON_ESize))
+        for j in range(1,CON_ESize):
+            _e_minus[j][j-1] = 1.0
+        _e_minus[0][0] = 1.0
+        return _e_minus
+
+    if act == A_IDLE:
+        return E_zero_mat()
+    elif act == A_GETE:
+        return E_plus_mat()
+    elif act == A_QH or act == A_QL:
+        return E_minus_mat()
+
 
 def Get_Q_mat(HorL, act):
+    # matrix for Q high and Q low
+    def Q_plus_mat(qlen, inj):
+        _q_plus = np.array([[0.0 for _ in range(qlen)] for _ in range(qlen)])
+        for i in range(qlen-1):
+            _q_plus[i][i] = 1.0 - inj
+            _q_plus[i][i+1] = inj
+        _q_plus[qlen-1][qlen-1] = 1.0
+        return _q_plus
+    
+    def Q_zero_mat(qlen):
+        return np.identity(qlen, float)
+        
+    def Q_minus_mat(qlen):    
+        _q_minus = np.array([[0.0 for _ in range(qlen)] for _ in range(qlen)])
+        for j in range(1,qlen):
+            _q_minus[j][j-1] = 1.0
+        _q_minus[0][0] = 1.0    
+        return _q_minus
+        
     Q_H_mat_plus = Q_plus_mat(CON_QSize_H,CON_injec_high)
     Q_H_mat_zero = Q_zero_mat(CON_QSize_H)
     Q_H_mat_minus = Q_minus_mat(CON_QSize_H)
@@ -113,17 +107,15 @@ def Get_Q_mat(HorL, act):
     else:
         exit('Error in get_Q_mat()') 
 
-
-
 def Get_Overall_mat(act):
     _cmat = Get_C_mat(act)
-    _mmat = Get_M_mat(act)
     _emat = Get_E_mat(act)
     _qhmat = Get_Q_mat('H', act)
     _qlmat = Get_Q_mat('L', act)
-    overkron = np.kron(np.kron(np.kron(_cmat, _mmat), _emat), np.kron(_qhmat,_qlmat))
+    overall_kron = np.kron(np.kron(_cmat, _emat), np.kron(_qhmat,_qlmat))
 #     print overkron.shape
-    return overkron
+    return overall_kron
+
 
 def ElecPriceCost(_c):
     if _c<0 or _c>=CON_CSize:
@@ -131,36 +123,38 @@ def ElecPriceCost(_c):
         exit()
     
     if _c in SET_CHARGER:
-        return -np.power(_c,0.5)*1.0
+        return -np.power(_c, 0.5)*1.0
     else:
         return -65536.0
+
+def MessengerDeliveryProb(_c):
+    if _c in SET_MESSENGER:
+        # return np.power( (CON_CSize-1.0-_c)/(len(SET_MESSENGER)-1.0) , 0.5)
+        return 0.5
+    else:
+        return 0.0
 
 def QDelayCost(_qh, _ql):
     return -0.6*_qh -0.4*_ql
 
-def MessengerDeliveryProb(_m):
-    return np.power(_m/CON_MSize, 0.5)
 
-def Reward(_c, _m, _e, _qh, _ql, action):
+def Reward(_c, _e, _qh, _ql, action):
     if action == A_IDLE:
         return QDelayCost(_qh, _ql)
     elif action == A_GETE:
         return ElecPriceCost(_c) + QDelayCost(_qh, _ql)
     elif action == A_QH:
-        if _qh>0 and _e>0:
-            if _c in SET_MESSENGER:
-                return 2.0*MessengerDeliveryProb(_m) + QDelayCost(_qh, _ql)
-            else:
-                return -65535.0
+        if _qh>0 and _e>0 and (_c in SET_MESSENGER):
+            return 2.0*MessengerDeliveryProb(_c) + QDelayCost(_qh, _ql)
+#                 return 2.0 + QDelayCost(_qh, _ql)
         else:
-            return -65535.0
+            return -65535000.0
     elif action == A_QL:
-        if _ql>0 and _e>0:
-            if _c in SET_MESSENGER:
-                return 0.7*MessengerDeliveryProb(_m) + QDelayCost(_qh, _ql)
-            else:
-                return -65535.0
+        if _ql>0 and _e>0 and (_c in SET_MESSENGER):
+            return 1.0*MessengerDeliveryProb(_c) + QDelayCost(_qh, _ql)
+#                 return 0.7 + QDelayCost(_qh, _ql)
         else:
-            return -65535.0
+            return -65535000.0
     else:
-        return QDelayCost(_qh, _ql)
+        print "error in Reward()"
+        exit()
--- socialeh.py
+++ socialeh.py
@@ -17,9 +17,9 @@ from head import *
 
 # a dirty function
 def Get_2Dlized_Result(_vi, _displist):
-    sizelist = [CON_CSize, CON_MSize, CON_ESize, CON_QSize_H, CON_QSize_L]
-    namelist = ['C', 'M', 'E', 'Q_H', 'Q_L']
-    easynamelist = ['Contact state C', 'Messenger state M', 'Energy state E', 'High queue Q_H', 'Low queue Q_L']
+    sizelist = [CON_CSize, CON_ESize, CON_QSize_H, CON_QSize_L]
+    namelist = ['C', 'E', 'Q_H', 'Q_L']
+    easynamelist = ['Contact state C', 'Energy state E', 'High queue Q_H', 'Low queue Q_L']
     if not len(_displist)==2:
         print "Error in Get_2Dlized_Result()"
         exit()
@@ -34,12 +34,11 @@ def Get_2Dlized_Result(_vi, _displist):
             loopset.append(sizelist[i])
             loopindset.append(i)
             
-    _LOOP1, _LOOP2, _LOOP3 = loopset
-    _LOOPIND1, _LOOPIND2, _LOOPIND3 = loopindset
+    _LOOP1, _LOOP2 = loopset
+    _LOOPIND1, _LOOPIND2 = loopindset
     
-    prod_left = np.array([0.,0.,0.,0.,0.])
-    prod_right = np.transpose(np.array([CON_MSize*CON_ESize*CON_QSize_H*CON_QSize_L, \
-                                       CON_ESize*CON_QSize_H*CON_QSize_L, \
+    prod_left = np.array([0.,0.,0.,0.])
+    prod_right = np.transpose(np.array([CON_ESize*CON_QSize_H*CON_QSize_L, \
                                        CON_QSize_H*CON_QSize_L, \
                                        CON_QSize_L, \
                                        1.0]))
@@ -47,22 +46,18 @@ def Get_2Dlized_Result(_vi, _displist):
     f = open('./MAT-'+str(dim1)+'-'+str(dim2),'w')
     for lp1 in range(_LOOP1):
         for lp2 in range(_LOOP2):
-            for lp3 in range(_LOOP3):
-                f.write( str(easynamelist[_LOOPIND1])+' = '+str(lp1)+'\n')
-                f.write( str(easynamelist[_LOOPIND2])+' = '+str(lp2)+'\n')
-                f.write( str(easynamelist[_LOOPIND3])+' = '+str(lp3)+'\n')
-                f.write( str(easynamelist[ind_dim1])+' as row var |\n')
-                f.write( str(easynamelist[ind_dim2])+' as row var ->\n')
-                for disp1 in range(sizelist[ind_dim1]):
-                    for disp2 in range(sizelist[ind_dim2]):
-                        prod_left[_LOOPIND1] = lp1
-                        prod_left[_LOOPIND2] = lp2
-                        prod_left[_LOOPIND3] = lp3
-                        prod_left[ind_dim1] = disp1
-                        prod_left[ind_dim2] = disp2
-                        _ind = int(prod_left.dot(prod_right))
-                        f.write( str(_vi.policy[_ind])+'   ')
-                    f.write('\n')
+            f.write( str(easynamelist[_LOOPIND1])+' = '+str(lp1)+'\n')
+            f.write( str(easynamelist[_LOOPIND2])+' = '+str(lp2)+'\n')
+            f.write( str(easynamelist[ind_dim1])+' as row var |\n')
+            f.write( str(easynamelist[ind_dim2])+' as row var ->\n')
+            for disp1 in range(sizelist[ind_dim1]):
+                for disp2 in range(sizelist[ind_dim2]):
+                    prod_left[_LOOPIND1] = lp1
+                    prod_left[_LOOPIND2] = lp2
+                    prod_left[ind_dim1] = disp1
+                    prod_left[ind_dim2] = disp2
+                    _ind = int(prod_left.dot(prod_right))
+                    f.write( str(_vi.policy[_ind])+'   ')
                 f.write('\n')
             f.write('\n')
         f.write('\n')
@@ -72,23 +67,21 @@ def Get_2Dlized_Result(_vi, _displist):
 R = np.zeros((CON_DIM,len(SET_A)))
 cnt = 0
 for ic in range(CON_CSize):
-    for im in range(CON_MSize):
-        for ie in range(CON_ESize):
-            for iqh in range(CON_QSize_H):
-                for iql in range(CON_QSize_L):
-                    for action in SET_A:
-                        R[cnt][action] = Reward(ic,im,ie,iqh,iql, action)
-                    cnt = cnt + 1
+    for ie in range(CON_ESize):
+        for iqh in range(CON_QSize_H):
+            for iql in range(CON_QSize_L):
+                for action in SET_A:
+                    R[cnt][action] = Reward(ic,ie,iqh,iql, action)
+                cnt = cnt + 1
 
 P = np.array([ Get_Overall_mat(A_IDLE), Get_Overall_mat(A_GETE), Get_Overall_mat(A_QH), Get_Overall_mat(A_QL) ])
 
-vi = mdptoolbox.mdp.ValueIteration(P, R, 0.99)
+vi = mdptoolbox.mdp.ValueIteration(P, R, 0.999)
 vi.run()
 print "Done"
 # print vi.policy
 Get_2Dlized_Result(vi,['C','Q_L'])
 Get_2Dlized_Result(vi,['C','Q_H'])
-Get_2Dlized_Result(vi,['M','Q_L'])
 Get_2Dlized_Result(vi,['Q_L','Q_H'])
 # print vi.iter
 # print vi.V
